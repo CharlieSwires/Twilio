4156505cce8654bcc4fd457def1e453c7a5eb9a1
charles_swires <charles.swires@vizionnetwork.co.uk>
2020-02-18 16:41:04 +0000

    Latest

--- TDemo/src/main/java/Twilio/TDemo/Main.java
+++ TDemo/src/main/java/Twilio/TDemo/Main.java
@@ -5,24 +5,25 @@ import java.net.URISyntaxException;
 
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Call;
+import com.twilio.twiml.voice.Say;
 import com.twilio.type.PhoneNumber;
 
 public class Main {
-    // Find your Account Sid and Token at twilio.com/console
-    public static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
-    public static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
+	// Find your Account Sid and Token at twilio.com/console
+	public static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
+	public static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
 
-    public static void main(String[] args) throws URISyntaxException {
-        String from = System.getenv("TWILIO_NUMBER");
-        String to = "+447596974113";
-        
-    	System.out.println("ACCOUNT_SID="+ACCOUNT_SID+" AUTH_TOKEN="+AUTH_TOKEN+" from="+from+" to="+to);
-        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+	public static void main(String[] args) throws URISyntaxException {
+		String from = System.getenv("TWILIO_NUMBER");
+		String to = "+447596974113";
 
+		System.out.println("ACCOUNT_SID="+ACCOUNT_SID+" AUTH_TOKEN="+AUTH_TOKEN+" from="+from+" to="+to);
+		Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
-        Call call = Call.creator(new PhoneNumber(to), new PhoneNumber(from),
-                new URI("http://demo.twilio.com/docs/voice.xml")).create();
+		Call call = Call.creator(new PhoneNumber(to), new PhoneNumber(from),
+				new URI("http://raw.githubusercontent.com/CharlieSwires/Twilio/master/TDemo/src/main/resources/voice.xml"))
+				.create();
 
-        System.out.println(call.getSid());
-    }
+		System.out.println(call.getSid());
+	}
 }
